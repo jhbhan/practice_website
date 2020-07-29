7277e2b766900d89440c2bb9a549a75666293fc0
jhbhan <jhbhan@gmail.com>
2020-07-29 07:04:08 -0700

    different height, changed tag name

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $("a.scrollLink").click(function (event) {
         event.preventDefault();
         $("html, body").animate(
-            { scrollTop: $($(this).attr("href")).offset().top - 100 },
+            { scrollTop: $($(this).attr("href")).offset().top - 50 },
             400
         );
     });
